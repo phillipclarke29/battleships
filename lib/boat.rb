60582784c053b2416ef20cbbde06e89f023ca65e
philip <philip.clarke@redhood.co.uk>
2015-09-07 13:40:38 +0100

    first boat

---
+++ lib/boat.rb
@@ -0,0 +1,10 @@
+class Boat
+
+  attr_reader :position
+
+  def initialize(x,y)
+    @position =[x,y]
+  end
+
+
+end
---
+++ spec/boat_spec.rb
@@ -0,0 +1,20 @@
+require 'boat'
+
+describe Boat do
+
+    let(:boat){Boat.new(1,2)}
+
+  it 'should initialize with an x and a y coordinate' do
+    expect(Boat).to respond_to(:new).with(2).argument
+  end
+
+  it 'should have a position' do
+    expect(boat.position.class).to eql(Array)
+  end
+
+
+
+
+
+
+end
