2cd2050edb6d868c5f053f152c1f92cda99f7dad
philip <philip.clarke@redhood.co.uk>
2015-09-07 16:13:56 +0100

    created player

---
+++ lib/player.rb
@@ -0,0 +1,11 @@
+
+
+class Player
+
+def boats
+@boats = []
+end
+
+
+
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,14 @@
+require 'player'
+
+describe Player do
+
+  it 'contain an array of boats' do
+    expect(subject).to respond_to(:boats)
+
+  end
+
+
+
+
+
+end
